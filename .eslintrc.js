44ba94dd0777dd839ef5321f227622c94c16c2ea
Felix Sargent <fsargent@users.noreply.github.com>
2018-05-31 12:37:59 -0400

    Big refactor to use maps_lib and remove the javascript. (#15)
    * Attempts at iframes
    
    * Linting and reformatting, simplifying a lot of function code.
    
    * Very stripped down version around an iframe
    
    * Refactor to use new 1.4 version of maps_lib.

---
+++ .eslintrc.js
@@ -0,0 +1,14 @@
+
+module.exports = {
+    "env": {
+        "browser": true,
+        "jquery": true
+    },
+    "globals":{
+        "map": true,
+        "google": true,
+        "geocoder": true
+    },
+    // "rules": {"no-multi-str": false},
+    "extends": "airbnb-base"
+};
